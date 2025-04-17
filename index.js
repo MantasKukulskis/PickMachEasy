99250ad9951bf5210d7d6fbdf82843c5dd2bbdff
Mantas Kukulskis <8mantas8@gmail.com>
2025-04-17 09:13:54 +0300

    init

---
+++ index.js
@@ -0,0 +1,6 @@
+const board = document.getElementById("game-board");
+const startBtn = document.getElementById("startBtn");
+const difficultySelect = document.getElementById("difficulty");
+const statusEl = document.getElementById("status");
+
+const emojis = ["🐶", "🍕", "🚗", "🌈", "🐱", "🍎", "🎵", "⚽"];
